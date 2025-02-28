67ae7de7a7edc1b69e90bbbf7b766498717b317f
Machado, Gabriel <gabrielgamerlive@gmail.com>
2025-02-28 15:11:53 -0300

    correção para teste da aula
    realizado correção para funcionar com o @Autowired

--- src/main/java/br/com/diferpan/aulasdev/machado/desafio01/services/ShippingService.java
+++ src/main/java/br/com/diferpan/aulasdev/machado/desafio01/services/ShippingService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShippingService {
 
-    double shippingValue;
+    private double shippingValue;
 
 
     public double shipment(Order order){
